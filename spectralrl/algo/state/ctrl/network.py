c5848387c439a535f840fbdcf6e79db8136dddb3
typoverflow <typoverflow@outlook.com>
2025-02-19 15:36:18 +0800

    update: ctrl networks

--- spectralrl/algo/state/ctrl/network.py
+++ spectralrl/algo/state/ctrl/network.py
@@ -5,11 +5,6 @@ import torch.nn.functional as F
 from spectralrl.module.net.mlp import MLP
 
 
-class Sin(nn.Module):
-    def forward(self, x):
-        return torch.sin(x)
-    
-    
 class Phi(nn.Module):
     def __init__(
         self,
@@ -43,36 +38,66 @@ class Mu(nn.Module):
         return out
 
 
+class Theta(nn.Module):
+    def __init__(
+        self,
+        feature_dim,
+        hidden_dims,
+    ) -> None:
+        super().__init__()
+        self.mlp = MLP(feature_dim, 1, hidden_dims, activation=nn.ELU, norm_layer=nn.LayerNorm)
+
+    def forward(self, feature):
+        return self.mlp(feature)
+
+
+class RFFLayer(nn.Module):
+    def __init__(
+        self,
+        feature_dim: int,
+        hidden_dim: int,
+        learnable: bool = True
+    ):
+        super().__init__()
+        self.learnable = learnable
+        if learnable:
+            self.layer = nn.Linear(feature_dim, hidden_dim)
+        else:
+            self.register_buffer("noise", torch.randn([feature_dim, hidden_dim], requires_grad=False))
+
+    def forward(self, x):
+        if self.learnable:
+            return torch.sin(self.layer(x))
+        else:
+            return torch.sin(self.la)
+
+
 class RFFCritic(nn.Module):
-    def __init__(self,
+    def __init__(
+        self,
         feature_dim: int,
         hidden_dim: int
     ):
         super().__init__()
-        self.ln = nn.LayerNorm(feature_dim)
-        self.l1 = nn.Linear(feature_dim, hidden_dim)
-        self.ln1 = nn.LayerNorm(hidden_dim)
-        self.l2 = nn.Linear(hidden_dim, hidden_dim)
-        self.ln2 = nn.LayerNorm(hidden_dim)
-        self.l3 = nn.Linear(hidden_dim, 1)
+        self.net1 = nn.Sequential(
+            nn.LayerNorm(feature_dim),
+            RFFLayer(feature_dim, hidden_dim, learnable=True),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.LayerNorm(hidden_dim),
+            nn.ELU(),
+            nn.Linear(hidden_dim, 1)
+        )
 
-        self.l4 = nn.Linear(feature_dim, hidden_dim)
-        self.ln4 = nn.LayerNorm(hidden_dim)
-        self.l5 = nn.Linear(hidden_dim, hidden_dim)
-        self.ln5 = nn.LayerNorm(hidden_dim)
-        self.l6 = nn.Linear(hidden_dim, 1)
+        self.net2 = nn.Sequential(
+            nn.LayerNorm(feature_dim),
+            RFFLayer(feature_dim, hidden_dim, learnable=True),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.LayerNorm(hidden_dim),
+            nn.ELU(),
+            nn.Linear(hidden_dim, 1)
+        )
 
-    def forward(self, x, return_feature=False):
-        q1_feature, q2_feature = self.forward_feature(x)
-        q1 = self.l3(q1_feature)
-        q2 = self.l6(q2_feature)
+    def forward(self, x):
+        q1 = self.net1(x)
+        q2 = self.net2(x)
         return torch.stack([q1, q2], dim=0)
-
-    def forward_feature(self, x):
-        x = self.ln(x)
-        q1 = torch.sin(self.ln1(self.l1(x)))
-        q1 = torch.nn.functional.elu(self.ln2(self.l2(q1)))
-
-        q2 = torch.sin(self.ln4(self.l4(x)))
-        q2 = torch.nn.functional.elu(self.ln5(self.l5(q2)))
-        return q1, q2
